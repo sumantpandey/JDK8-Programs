335f9876c67f330448524944453ae9da5e564608
pandeysu <sumantkumar.pandey@arhs-developments.com>
2018-10-15 16:28:46 +0200

    added new classes

---
+++ src/main/java/JAXBDemo.java
@@ -0,0 +1,17 @@
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+
+public class JAXBDemo {
+
+    public static void main(String args[]) throws JAXBException {
+        final Item dvd = new Item(101, "Lord of the Rings", 10);
+        final JAXBContext context = JAXBContext.newInstance(Item.class);
+        final Marshaller marshaller = context.createMarshaller();
+        StringWriter writer = new StringWriter();
+        marshaller.marshal(dvd, writer);
+        System.out.println(writer.toString());
+    }
+}
+
--- src/main/java/ParseXML.java
+++ src/main/java/ParseXML.java
@@ -1,38 +1,33 @@
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
-import javax.activation.MimetypesFileTypeMap;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParseXML {
-    static String xml = "<?xml version=\"1.0\" encoding=\"UTF-16\"?><message>HELLO!</message>";
-    static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-    static DocumentBuilder db = null;
+
     public static void main(String [] args) {
-        try {
-            db = dbf.newDocumentBuilder();
-            InputSource is = new InputSource();
-            is.setCharacterStream(new StringReader(xml));
-            try {
-                Document doc = db.parse(is);
-                String message = doc.getDocumentElement().getTextContent();
-                System.out.println("content type is "+doc.getXmlEncoding());
-                System.out.println("doctype is "+ doc.getDoctype());
-                System.out.println(message);
-                new MimetypesFileTypeMap().getContentType(doc));
-            } catch (SAXException e) {
-                // handle SAXException
-            } catch (IOException e) {
-                // handle IOException
-            }
-        } catch (
-                ParserConfigurationException e1) {
-            // handle ParserConfigurationException
-        }
+        /*ListData listdata = new ListData();
+        List lst = new ArrayList();
+        DataItem d1 = new DataItem();
+        d1.setVal("data1");
+        DataItem d2 = new DataItem();
+        d2.setVal("data2");
+        lst.add(d1);
+        lst.add(d2);
+        listdata.setList(lst);
+        callMethod(listdata);
+        for (DataItem dataItem: listdata.getList())
+        {
+            System.out.println(dataItem.getVal());
+        }*/
+
+    }
+    static void callMethod(ListData listData)
+    {
+        /*List<DataItem> ll = listData.getList();
+        for (DataItem dataItem: ll)
+        {
+            dataItem.setVal("changedVal1");
+        }*/
     }
 }
