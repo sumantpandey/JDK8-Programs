4e815f526b91322d88e8c058af305d95b780dd99
pandeysu <sumantkumar.pandey@arhs-developments.com>
2018-10-16 15:51:16 +0200

    parsing java object backto xml using jaxb

---
+++ src/main/java/DataItem.java
@@ -0,0 +1,14 @@
+public class DataItem {
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+    String val;
+    //////comment to test rebase
+
+}
