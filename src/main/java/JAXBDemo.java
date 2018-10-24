987c1ec63ad514b783cb77122ed3837968ae4098
pandeysu <sumantkumar.pandey@arhs-developments.com>
2018-10-25 01:08:16 +0200

    commit in master

--- src/main/java/JAXBDemo.java
+++ src/main/java/JAXBDemo.java
@@ -15,6 +15,7 @@ public class JAXBDemo {
         System.out.println("testing git rebase, made change in master then will rebase mastercopy1 branch onto master, lets see");
         System.out.println("1111111111111111");
         System.out.println("2222222222222222222");
+        System.out.println("333333333333333");
     }
 }
 
