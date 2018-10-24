ac8d7a5b7e08d606da7715f02886b75ae3b1c975
pandeysu <sumantkumar.pandey@arhs-developments.com>
2018-10-25 01:12:57 +0200

    revert 4 commits in master

--- src/main/java/JAXBDemo.java
+++ src/main/java/JAXBDemo.java
@@ -13,10 +13,7 @@ public class JAXBDemo {
         marshaller.marshal(dvd, writer);
         System.out.println(writer.toString());
         System.out.println("testing git rebase, made change in master then will rebase mastercopy1 branch onto master, lets see");
-        System.out.println("1111111111111111");
-        System.out.println("2222222222222222222");
-        System.out.println("333333333333333");
-        System.out.println("444444444444444");
+
     }
 }
 
